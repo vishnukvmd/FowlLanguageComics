ec49d62dcd96707dcf9cdf1369f5e72a79bc7d5b
Vishnu <vishnumohandas@gmail.com>
2015-09-17 11:42:03 +0530

    Minor update to styles

--- app/src/main/java/tech/vishnu/fowllaguagecomics/ui/MainActivity.java
+++ app/src/main/java/tech/vishnu/fowllaguagecomics/ui/MainActivity.java
@@ -1,7 +1,6 @@
 package tech.vishnu.fowllaguagecomics.ui;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
