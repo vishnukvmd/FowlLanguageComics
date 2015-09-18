6db3c9a9bdae11a8deaf1d28a33e59fe59ba69c8
Vishnu <vishnumohandas@gmail.com>
2015-09-18 09:49:42 +0530

    Add Fabric

--- app/src/main/java/com/fowllanguagecomics/android/FLCApplication.java
+++ app/src/main/java/com/fowllanguagecomics/android/FLCApplication.java
@@ -3,8 +3,11 @@ package com.fowllanguagecomics.android;
 import android.app.Application;
 import android.util.Log;
 
+import com.crashlytics.android.Crashlytics;
 import com.parse.Parse;
 import com.parse.ParseInstallation;
+
+import io.fabric.sdk.android.Fabric;
 
 public class FLCApplication extends Application {
     private static final String PARSE_APPLICATION_ID = "iSsDzFH66ybndoGKzWVr9RH6P7KYwsscDVWVkAYr";
@@ -16,5 +19,6 @@ public class FLCApplication extends Application {
         Parse.setLogLevel(Log.VERBOSE);
         Parse.initialize(this, PARSE_APPLICATION_ID, PARSE_CLIENT_KEY);
         ParseInstallation.getCurrentInstallation().saveInBackground();
+        Fabric.with(this, new Crashlytics());
     }
 }
